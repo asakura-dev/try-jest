a0bfd014477c2214a19bc2d596ab08bba527ff18
asakura-dev <asakuradev@gmail.com>
2019-12-31 02:43:54 +0900

    P15

--- src/tests/money.test.ts
+++ src/tests/money.test.ts
@@ -3,7 +3,9 @@ import Dollar from "../dollar";
 describe('money', (): void => {
   test('test multiplication', (): void => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toEqual(10);
+    const product = five.times(2);
+    expect(product.amount).toEqual(10);
+    const product2 = five.times(3);
+    expect(product2.amount).toEqual(15);
   });
 });
