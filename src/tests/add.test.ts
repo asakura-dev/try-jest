7e5b41e04c1a8b5c09f6265c0fd68eb84ee01ed5
asakura-dev <asakuradev@gmail.com>
2020-01-01 21:56:55 +0900

    Remove add.ts and test

--- src/tests/add.test.ts
+++
@@ -1,8 +0,0 @@
-import add from "../add";
-
-describe("add", (): void => {
-  test("should return 3", (): void => {
-    const result: number = add(1, 2);
-    expect(result).toEqual(3);
-  });
-});
